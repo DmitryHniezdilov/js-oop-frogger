92bb53125bfacbee519efcb9b6c4d2423f1429d6
DmitryHniezdilov <hniezdilovd@gmail.com>
2022-09-02 15:45:24 +0300

    feat: added render enemies

--- js/app.js
+++ js/app.js
@@ -1,19 +1,33 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+"use strict";
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+const fieldWidth = 400,
+    fieldHeight = 390,
+    enemyWidth = 100,
+    enemySpeed = 200,
+    enemyImg = 'images/enemy-bug.png';
+
+const Enemy = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = enemyImg;
+    this.speed = enemySpeed;
+    this.enemyWidth = enemyWidth;
+    this.fieldWidth = fieldWidth;
+
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.x += this.speed * dt;
+    console.log('this.x', this.x);
+
+    console.log('fieldWidth', fieldWidth + enemyWidth);
+
+    if (this.x > fieldWidth + enemyWidth) {
+        console.log('if fieldWidth', fieldWidth);
+        this.x = -enemyWidth;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -30,7 +44,25 @@ Enemy.prototype.render = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+const heightLinesOfObstacles = {
+    1: 62,
+    2: 144,
+    3: 226
+};
 
+const allEnemies = [];
+
+const createEnemies = function(n, row) {
+    const rowNumber = heightLinesOfObstacles[row];
+
+    for (let i = 0; i < n; i++) {
+        allEnemies.push(new Enemy(0, rowNumber));
+    }
+}
+
+createEnemies(1, 1);
+createEnemies(2, 2);
+createEnemies(3, 3);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
